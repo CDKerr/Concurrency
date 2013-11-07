f847fd3cfe6948f823072a0a1fcb276b83193269
CDKerr <christopherkerr@live.com.au>
2013-11-07 15:47:45 +0000

    7th commit

---
+++ UsingThreadSafeNavigableMaps/src/Contact.java
@@ -0,0 +1,20 @@
+
+public class Contact {
+	
+	private String phone, name;
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Contact(String phone, String name) {
+		this.phone = phone;
+		this.name = name;
+	}
+
+
+}
